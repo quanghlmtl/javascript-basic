cbf03ea3e66993d4fcfced384e6cad9c11977ec9
quanghtml <quangviphungloi@gmail.com>
2023-09-08 22:14:58 +0700

    sourcecode string js

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log('Hello word')
+//javascript data types: var or let(khuyên dùng)
+let a = 1;
+let b = -2;
+let c = 1.3;
+let d = 4, e = 6.4, f = 343;
+
+//javaScript strings
+//javaScript cho phep dung 3 kieu dau nhay trong string "" '' ``
+let s1 = "abc";
+let s2 = 'i love "Huyen My"""';
+let s3 = `ghi`;
+// s1, s2, s3 đều đúng
+console.log("Test number ",'x = ', a,";b =", b,`;cd = `, c + d)
+console.log(s2)
